5b76667810d6a3ebe304f5e526c1c92992e875e5
Arnaud BODEL <contact@osw3.net>
2020-07-30 11:03:06 +0200

    -

--- 04-animations/assets/js/script.js
+++ 04-animations/assets/js/script.js
@@ -1,3 +1,8 @@
 // Doc jQuery pour afficher et masquer les blocks 1 et 2
 
-$('#btnOpenBlock1');
+$('#btnOpenBlock1').on('click', function(){
+    $('#block1').show();
+});
+$('#btnCloseBlock1').on('click', function(){
+    $('#block1').hide();
+});
