066c46c3e78ed2725a67a28a946246cd98ad7a45
Arnaud BODEL <contact@osw3.net>
2020-07-30 12:33:19 +0200

    -

---
+++ TP/tp-01/assets/js/script.js
@@ -0,0 +1,21 @@
+var heros = [
+    {
+        nickname: "Batman",
+        firstname: "Bruce",
+        lastname: "WAYNE",
+        location: "Gotham",
+        powers: ['Belt', 'Money']
+    },
+    // ... 
+];
+
+
+// jQuery
+// > each() : pour gerer les boucles
+// > append() : Ajouter du contenu dans un element HTML
+// > html() : Remplacer du contenu dans un element HTML
+// Créer un element html avec jquery
+
+// Vanilla JS
+// > join() : pour convertir un tableau en chaine
+
